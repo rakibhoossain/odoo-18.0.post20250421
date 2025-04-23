33d6636c95f5f31e0a1acac0dec6c50da74a37b5
Rakib Hossain <rakibhoossain@gmail.com>
2025-04-23 15:09:42 +0600

    advertise menu

--- odoo/addons/estate/__manifest__.py
+++ odoo/addons/estate/__manifest__.py
@@ -5,6 +5,7 @@
     'author': "Rakib Hossain",
     'data': [
         'security/ir.model.access.csv',
-        'views/estate_property_views.xml'
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
 }
