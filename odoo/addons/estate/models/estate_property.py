4bdfecdc4b058bea9d296dfedf6feb7a67ec1c5a
Rakib Hossain <rakibhoossain@gmail.com>
2025-04-23 16:20:46 +0600

    customize view

--- odoo/addons/estate/models/estate_property.py
+++ odoo/addons/estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from odoo.exceptions import UserError, ValidationError
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
@@ -7,9 +8,9 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Html("Vehicle Description")
     postcode = fields.Char()
-    date_availability = fields.Date("Date Availability")
+    date_availability = fields.Date("Date Availability", default=fields.Datetime.now)
     expected_price = fields.Float(help="Expected Price", required=True)
-    selling_price = fields.Float("Selling Price")
+    selling_price = fields.Float("Selling Price", readonly=True)
     bedrooms = fields.Float("Bedrooms")
     living_area = fields.Float("Living Area")
     facades = fields.Float("Facades")
@@ -20,3 +21,28 @@ class EstateProperty(models.Model):
         string='Type',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="Garden orientation")
+
+    state = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+        string="Status",
+        required=True,
+        copy=False,
+        default="new",
+    )
+    active = fields.Boolean("Active", default=True)
+
+    def action_sold(self):
+        if "canceled" in self.mapped("state"):
+            raise UserError("Canceled properties cannot be sold.")
+        return self.write({"state": "sold"})
+
+    def action_cancel(self):
+        if "sold" in self.mapped("state"):
+            raise UserError("Sold properties cannot be canceled.")
+        return self.write({"state": "canceled"})
